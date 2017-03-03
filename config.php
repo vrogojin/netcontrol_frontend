925e10267197c567c753485515bd195117163ae4
Vladimir Rogojin <vrogojin@abo.fi>
2017-03-03 11:35:03 +0200

    added about page. fixed menu and footer for all the pages

--- config.php
+++ config.php
@@ -16,276 +16,21 @@ $head='
 	<meta http-Equiv="Cache-Control" Content="no-cache">
 	<meta http-Equiv="Pragma" Content="no-cache">
 	<meta http-Equiv="Expires" Content="0">
+	<link rel="stylesheet"  href="nc4bm.css" type="text/css" media="screen" />
 ';
 
 
-$style='
-    html{
-    background-color: #f3f3f3;
-    }
-
-    .main-content{
-    margin-top: 20px;
-    margin-bottom: 20px;
-    }
-
-    .form-labels-on-top{
-        box-sizing: border-box;
-        max-width: 700px;
-        margin: 0 auto;
-        padding: 55px;
-
-        background-color:  #ffffff;
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1);
-
-        font: bold 14px sans-serif;
-        text-align: center;
-    }
-
-    header{
-    box-sizing: border-box;
-    text-align: center;
-    width: 100%;
-    padding: 25px 40px;
-    background-color: #34495e;
-    overflow: hidden;
-    margin: 0 auto;
-    }
-
-
-    header h1{
-        float: left;
-        font: normal 24px/1.5 \'Open Sans\', sans-serif;
-        color: #fff;
-    }
-
-    header a{
-        color:#fff;
-        float: right;
-        text-decoration: none;
-        display: inline-block;
-        width: 150px;
-        height: 15px;
-        padding: 13px 0px;
-        border-radius: 3px;
-        font: bold 14px/1 \'Open Sans\', sans-serif;
-        background-color:#27ae60;
-        margin: auto;
-        margin-left: 5px;
-        margin-top: 5px;
-
-    }
-
-    .form-labels-on-top .form-row{
-        text-align: left;
-        max-width: 405px;
-        margin: 25px auto 0;
-    }
-
-    .form-labels-on-top .form-title-row{
-        margin: 0 auto 40px;
-    }
-
-    .form-labels-on-top h1{
-        display: inline-block;
-        box-sizing: border-box;
-        color: #4C565E;
-        font-size: 24px;
-        padding: 0 0 12px 0;
-        margin: 0;
-        border-bottom: 2px solid #6CAEE0;
-    }
-
-    .form-labels-on-top .form-row > label span{
-        display: block;
-        box-sizing: border-box;
-        color:  #5f5f5f;
-        width: 400px;
-        padding: 0 0 12px;
-    }
-
-    .form-labels-on-top input{
-    color:  #5f5f5f;
-    box-sizing: border-box;
-    width: 100%;
-    max-width: 405px;
-    box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.08);
-    padding: 12px 18px;
-    border: 1px solid #dbdbdb;
-    
-    }
-
-    .form-labels-on-top .form-row .form-radio-buttons {
-        width: auto;
-    }
-
-    .form-labels-on-top .form-row .form-check-box {
-        width: auto;
-    }
-
-    .form-labels-on-top textarea{
-        color:  #5f5f5f;
-        box-sizing: border-box;
-        width: 100%;
-        height: 200px;
-        box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.08);
-        border: 1px solid #dbdbdb;
-        margin: auto;
-    }
-
-    .form-labels-on-top input[type=radio],
-    .form-labels-on-top input[type=checkbox]{
-        box-shadow: none;
-        width: auto;
-    }
-
-    .form-labels-on-top select{
-        background-color: #ffffff;
-        color:  #5f5f5f;
-        box-sizing: border-box;
-        width: 100%;
-        box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.08);
-        padding: 12px 18px;
-        border: 1px solid #dbdbdb;
-    }
-
-    .form-labels-on-top .form-radio-buttons > div{
-        margin-bottom: 10px;
-    }
-
-    .form-labels-on-top .form-radio-buttons label span{
-        margin-left: 8px;
-        color:  #5f5f5f;
-        font-weight: normal;
-    }
-
-    .form-labels-on-top .form-check-box label span{
-        margin-left: 8px;
-        padding-top: 0px;
-        color:  #5f5f5f;
-        font-weight: normal;
-    }
-
-    .form-labels-on-top .form-radio-buttons input{
-        width: auto;
-    }
-
-    .form-labels-on-top input[type=submit]{
-        border-radius: 2px;
-        background-color:  #6caee0;
-        color: #ffffff;
-        font-weight: bold;
-        box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.08);
-        padding: 14px 22px;
-        border: 0;
-        margin-top: 15px;
-    }
-
-    .footer-distributed{
-    background-color: #292c2f;
-    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
-    box-sizing: border-box;
-    width: 100%;
-    text-align: left;
-    font: bold 16px sans-serif;
-
-    padding: 55px 50px;
-    margin-top: 20px;
-    }
-
-    .footer-distributed .footer-center,
-    .footer-distributed .footer-right{
-        display: inline-block;
-        vertical-align: top;
-    }
-
-
-    .footer-distributed h3 span{
-        color:  #5383d3;
-    }
-
-    /* Footer links */
-
-
-    .footer-distributed a{
-        display:inline-block;
-        float: left; 
-        clear:left;
-        line-height: 1.8;
-        text-decoration: none;
-        color:  inherit;
-    }
-
-    .footer-distributed a:hover{
-        color:  #e6e6e6;
-    }
-
-
-    /* Footer Center */
-
-    .footer-distributed .footer-center{
-        width: 50%;
-    }
-
-    .footer-distributed .footer-center img{
-        background-color:  #33383b;
-        color: #ffffff;
-        font-size: 25px;
-        width: 38px;
-        height: 38px;
-        border-radius: 50%;
-        text-align: center;
-        line-height: 42px;
-        margin: 10px 15px;
-        vertical-align: middle;
-    }
-
-
-    .footer-distributed .footer-center p{
-        display: inline-block;
-        color: #ffffff;
-        vertical-align: middle;
-        margin:0;
-    }
-
-    .footer-distributed .footer-center p span{
-        display:block;
-        font-weight: normal;
-        font-size:14px;
-        line-height:2;
-    }
-
-
-    /* Footer Right */
-
-    .footer-distributed .footer-right{
-        width: 20%;
-    }
-
-    .footer-distributed .footer-company-about{
-        line-height: 20px;
-        color:  #92999f;
-        font-size: 13px;
-        font-weight: normal;
-        margin: 0;
-    }
-
-    .footer-distributed .footer-company-about span{
-        display: block;
-        color:  #ffffff;
-        font-size: 14px;
-        font-weight: bold;
-        margin-bottom: 20px;
-    }
-';
+$style='';
 
 // The header HTML code
 $header='
     <header>
         <h1>NetControl4BioMed: Network Controllability for Biomedicine</h1>
-        <a href="documentation.html">Documentation</a>
-        <a href="download.html">Download</a>
-        <a href="">Home</a>
+        <a href="documentation.html" target="_new">Documentation</a>
+        <a href="download.html" target="_new">Download</a>
+	<a href="http://combio.abo.fi/research/network-controlability-project/" target="_new">Network Controlability</a>
+        <a href="about.html"  onclick="window.open(\'about.html\', \'newwindow\', \'width=700, height=520\'); return false;">About</a>
+	<a href=".">Home</a>
     </header>
     <div class=\"main-content\">
 ';
@@ -297,37 +42,47 @@ $footer='
             <div class="footer-center">
 
                 <div>
-                    <i class="tucs logo"><img id="tucs" src="/net_control/img/TUCS_logo.png"/></i>
+	    <a href="http://www.tucs.fi" target="_new">
+                    <i class="tucs logo"><img id="tucs" src="img/TUCS_logo.png"/></i>
                     <p><span>TUCS</span></p>
+	    </a>
                 </div>
 
                 <div>
-                    <i class="abo logo"><img id="abo" src="/net_control/img/ABO_logo.png"/></i>
+	    <a href="http://www.abo.fi" target="_new">
+                    <i class="abo logo"><img id="abo" src="img/ABO_logo.png"/></i>
                     <p><span>Åbo Akademi</span></p>
+	    </a>
                 </div>
 
                 <div>
-                    <i class="acm logo"><img id="acm" src="/net_control/img/AKA_logo.png"/></i>
+	    <a href="http://www.aka.fi" target="_new">
+                    <i class="acm logo"><img id="acm" src="img/AKA_logo.png"/></i>
                     <p><span>Academy of Finland</span></p>
+	    </a>
                 </div>
 
                 <div>
-                    <i class="tekes logo"><img id="tekes" src="/net_control/img/TEKES_logo.png"/></i>
+	    <a href="http://www.tekes.fi" target="_new">
+                    <i class="tekes logo"><img id="tekes" src="img/TEKES_logo.png"/></i>
                     <p><span>Tekes</span></p>
+	    </a>
                 </div>
                 
                 <div>
-                    <i class="cimo logo"><img id="cimo" src="/net_control/img/CIMO_logo.png"/></i>
+	    <a href="http://www.cimo.fi" target="_new">
+                    <i class="cimo logo"><img id="cimo" src="img/CIMO_logo.png"/></i>
                     <p><span>CIMO</span></p>
+	    </a>
                 </div>
             </div>
 
             <div class="footer-right">
 
                 <p class="footer-company-about">
-                    <span>About</span>
-                    <a href="http://">Contributers</a>
-                    <a href="http://">References</a>
+	    <a href="about.html"  onclick="window.open(\'about.html\', \'newwindow\', \'width=700, height=520\'); return false;"><span>About</span></a>
+	    <a href="http://combio.abo.fi/nc/net_control/contributors.html"  onclick="window.open(\'http://combio.abo.fi/nc/net_control/contributors.html\', \'newwindow\', \'width=400, height=250\'); return false;">Contributors</a>
+                    <a href="http://combio.abo.fi/nc/net_control/references.html"  onclick="window.open(\'http://combio.abo.fi/nc/net_control/references.html\', \'newwindow\', \'width=700, height=520\'); return false;">References</a>
                 </p>
 
             </div>
@@ -340,7 +95,6 @@ $footer='
                 </p>
 
             </div>
-
     </footer>
 ';
 
